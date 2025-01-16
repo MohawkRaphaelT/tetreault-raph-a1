39887a1b06b19369971b5932887bd96ee2d73761
MohawkRaphaelT <150467892+MohawkRaphaelT@users.noreply.github.com>
2025-01-16 11:48:54 -0500

    Add project

---
+++ m1b2-rpg/Program.cs
@@ -0,0 +1,22 @@
+﻿// Get player name, remember it
+Console.WriteLine("Hello traveller! What is your name?");
+string playerName = Console.ReadLine();
+
+// Get player gold
+Console.WriteLine("How much gold do you have?");
+int goldAmount = int.Parse(Console.ReadLine());
+
+//
+Console.WriteLine("We have your bronze sword ready. That will be 15 gold.");
+if (goldAmount >= 15)
+{
+    Console.WriteLine($"Thank you for your purchase!");
+    // Deduct gold
+    goldAmount -= 15;
+    // Update player on gold count
+    Console.WriteLine($"You have {goldAmount} gold left.");
+}
+else
+{
+    Console.WriteLine($"{playerName}, you don't have enough gold!");
+}
